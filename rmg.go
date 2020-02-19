a3d0a4b9fc65e291b71499d52b0c1ebd3404c101
liikii <609957891@qq.com>
2020-02-19 22:42:17 +0800

    up

---
+++ rmg.go
@@ -0,0 +1,47 @@
+package main
+
+import "os"
+import "flag"
+import "log"
+import "fmt"
+import "image"
+import _ "image/jpeg"
+import "image/png"
+import "github.com/nfnt/resize"
+
+
+func img_resize(fp string, w uint, h uint) {
+    file, err := os.Open(fp)
+    defer file.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+    img, _, err := image.Decode(file)
+    if err != nil {
+        log.Fatal(err)        
+    }
+    img2 := resize.Resize(w, h, img, resize.Bilinear)
+    out, err := os.Create("rmg_" + fp)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer out.Close()
+    // write new image to file
+    png.Encode(out, img2)
+    fmt.Println("rmg_" + fp + " ok")
+}
+
+
+func main() {
+    var w uint
+    var h uint
+    var fl string
+
+
+    flag.UintVar(&w, "w", 0, "max width")
+    flag.UintVar(&h, "h", 0, "max height")
+    flag.StringVar(&fl, "f", "", "images file path")
+    flag.Parse()
+
+    img_resize(fl, w, h)
+}
